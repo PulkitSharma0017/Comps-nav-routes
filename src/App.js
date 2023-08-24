d770f27b10c3112352af03d3672314d90543f4fe
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-08-24 18:01:44 +0530

    some refactoring of structure

--- src/App.js
+++ src/App.js
@@ -1,49 +1,7 @@
-import { GoBell, GoCloud, GoDatabase } from "react-icons/go";
-import Button from "./Button";
+import Accordion from "./components/Accordion";
 
 function App() {
-  const handleClick = () => {
-    console.log("Click !!!");
-  };
-
-  return (
-    <div>
-      <div>
-        <Button primary rounded className="mb-5" onClick={handleClick}>
-          <GoBell />
-          Click Here!
-        </Button>
-      </div>
-      <div>
-        <Button secondary className="mb-5">
-          Click Here!
-        </Button>
-      </div>
-      <div>
-        <Button success onMouseLeave={handleClick}>
-          <GoDatabase />
-          Click Here!
-        </Button>
-      </div>
-      <div>
-        <Button
-          warning
-          rounded
-          className="mb-5"
-          outlined
-          onMouseEnter={handleClick}
-        >
-          <GoCloud />
-          Click Here!
-        </Button>
-      </div>
-      <div>
-        <Button danger outlined>
-          Click Here!
-        </Button>
-      </div>
-    </div>
-  );
+  return <App />;
 }
 
 export default App;
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,49 @@
+import { GoBell, GoCloud, GoDatabase } from "react-icons/go";
+import Button from "../components/Button";
+
+function ButtonPage() {
+  const handleClick = () => {
+    console.log("Click !!!");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button primary rounded className="mb-5" onClick={handleClick}>
+          <GoBell />
+          Click Here!
+        </Button>
+      </div>
+      <div>
+        <Button secondary className="mb-5">
+          Click Here!
+        </Button>
+      </div>
+      <div>
+        <Button success onMouseLeave={handleClick}>
+          <GoDatabase />
+          Click Here!
+        </Button>
+      </div>
+      <div>
+        <Button
+          warning
+          rounded
+          className="mb-5"
+          outlined
+          onMouseEnter={handleClick}
+        >
+          <GoCloud />
+          Click Here!
+        </Button>
+      </div>
+      <div>
+        <Button danger outlined>
+          Click Here!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
