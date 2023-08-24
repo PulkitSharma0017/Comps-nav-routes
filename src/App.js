4d913a16c44cf0c0dbed87dd0e7ae51c3a7b0a1a
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-08-24 18:12:51 +0530

    render accordion data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import Accordion from "./components/Accordion";
 
 function App() {
-  return <App />;
+  const items = [
+    {
+      id: "hwsg5470",
+      label: "Can I use React on a project?",
+      content:
+        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
+    },
+    {
+      id: "hwsg5471",
+      label: "Can I use JavaScript on a project?",
+      content:
+        "You can use JavaScript on any project you want. You can use JavaScript on any project you want. You can use JavaScript on any project you want. You can use JavaScript on any project you want. You can use JavaScript on any project you want. You can use JavaScript on any project you want. You can use JavaScript on any project you want.",
+    },
+    {
+      id: "hwsg5472",
+      label: "Can I use CSS on a project?",
+      content:
+        "You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want.",
+    },
+  ];
+
+  return <Accordion items={items} />;
 }
 
 export default App;
