8c74f9a49a271dd30f8252da75f4c886d3ec2244
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-08-25 17:37:45 +0530

    display list, handle state changes, show icon

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const icon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
+
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div
+          onClick={() => {
+            handleClick(index);
+          }}
+        >
+          {icon}
+          {item.label}
+        </div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
