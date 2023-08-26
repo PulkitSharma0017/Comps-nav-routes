5ef90f56d043a93d57fb00ed6f2df33c86225941
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-08-27 02:11:51 +0530

    add props and styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,18 @@
 import ReactDOM from "react-dom";
 
-function Modal() {
+function Modal({ onClose, children, actionBar }) {
   return ReactDOM.createPortal(
     <div>
-      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
-      <div className="absolute inset-40 bg-white p-10">I'm a Modal</div>
+      <div
+        onClick={onClose}
+        className="absolute inset-0 bg-gray-300 opacity-80"
+      ></div>
+      <div className="absolute inset-40 bg-white p-10">
+        <div className="flex h-full flex-col justify-between">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
+      </div>
     </div>,
     document.querySelector(".modal-container"),
   );
