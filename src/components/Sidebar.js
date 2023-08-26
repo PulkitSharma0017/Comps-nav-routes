d304da5598798c24b0517d235c63a916adbd44f6
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-08-26 21:06:03 +0530

    create and add sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,33 @@
+import Link from "./Link";
+
+function Sidebar() {
+  const links = [
+    { label: "Dropdown", path: "/" },
+    { label: "Accordion", path: "/accordion" },
+    { label: "Buttons", path: "/buttons" },
+    { label: "Flex", path: "/flex" },
+    { label: "Table", path: "/table" },
+    { label: "Search", path: "/search" },
+  ];
+
+  const renderedLinks = links.map((link) => {
+    return (
+      <Link
+        key={link.label}
+        to={link.path}
+        className="mb-3"
+        activeClassName="font-bold border-l-4 border-blue-500 pl-2"
+      >
+        {link.label}
+      </Link>
+    );
+  });
+
+  return (
+    <div className="sticky top-0 flex flex-col items-start overflow-y-auto">
+      {renderedLinks}
+    </div>
+  );
+}
+
+export default Sidebar;
